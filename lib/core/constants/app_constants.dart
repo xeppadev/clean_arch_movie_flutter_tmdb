c270749370dd113cdee5e96882c29c4ac383f2ac
Cristhian Rodriguez <cprodriguez@unitru.edu.pe>
2024-09-20 22:00:11 -0500

    add new feature

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -2,5 +2,5 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppConstants {
   static final baseUrl = '${dotenv.env['URL']}';
-  static final apiToken = '${dotenv.env['API']}'; 
+  static final apiToken = '${dotenv.env['API']}';
 }
